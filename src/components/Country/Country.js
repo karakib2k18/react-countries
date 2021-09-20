edcf40c7a72ea03ab9ce89a98b67ffd3a2592021
karakib2k18 <karakib2k18@gmail.com>
2021-09-20 18:56:19 +0600

    second

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Country.css';
 const Country = (props) => {
     //console.log(props.country)
-    const { name, flag, area} = props.country;
+    const { name, flag, area, alpha2Code, capital, region, population} = props.country;
     return (
         <div className='country'>
             <img src={flag} alt="" style={{ height: '200px'}} />
-            <h3>NAME:{name}</h3>
+            <h2>NAME:{name}</h2>
             <h4>AREA: {area}</h4>
+            <h4>CODE:{alpha2Code}</h4>
+            <h4>CAPITAL: {capital}</h4>
+            <h4>REGION:{region}</h4>
+            <h4>POPULATION: {population}</h4>
         </div>
     );
 };
